5ada384e18209b6a8752125e27235ea5d1964e43
deepikamalle <131941605+deepikamalle@users.noreply.github.com>
2023-08-30 16:16:25 +0530

    Update StyledComponents.js

--- src/components/TimelineView/StyledComponents.js
+++ src/components/TimelineView/StyledComponents.js
@@ -1,26 +1,29 @@
 import styled from 'styled-components/macro'
 
-export const TimelineContainer = styled.div
+export const TimelineContainer = styled.div`
 display: flex;
 justify-content: space-around;
 align-items: center;
 min-height: 100vh;
+`
 
-export const ResponsiveContainer = styled.div
+export const ResponsiveContainer = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
 max-width:1110px;
 height: 100vh;
+`
 
-export const HeaderContainer = styled.div
+export const HeaderContainer = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
 margin-bottom: 30px;
+`
 
-export const Heading = styled.h1
+export const Heading = styled.h1`
 text-align: center;
 color: #171f46;
 font-family: 'Roboto';
@@ -31,8 +34,9 @@ margin: 0px;
 @media screen and (min-width: 768px) {
     font-size: 20px;
 }
+`
 
-export const CCBPHeading = styled.span
+export const CCBPHeading = styled.span`
 color: #2b237c;
 font-family: 'Roboto';
 font-weight : 500;
@@ -41,3 +45,4 @@ margin: 0px;
 @media screen and (min-width: 768px) {
     font-size: 30px;
 }
+`
